5cb5ff5885f8d87ac1292346506a61b831545670
ahnabrah <ahnabrah@iu.edu>
2025-09-18 13:59:04 -0400

    server.js

--- Stripe Integration/server.js
+++ Stripe Integration/server.js
@@ -1,13 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
 import paymentsRoute from "./routes/payments.js";
 import subscriptionsRoute from "./routes/subscriptions.js";
 import webhooksRoute from "./routes/webhooks.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-// JSON parser for normal routes
-app.use(bodyParser.json());
+// JSON parser
+app.use(express.json());
 
 // Routes
 app.use("/api/payments", paymentsRoute);
@@ -19,4 +21,6 @@ app.use("/webhook", webhooksRoute);
 // Static files for frontend
 app.use(express.static("public"));
 
-app.listen(4242, () => console.log("🚀 Server running on http://localhost:4242"));
+app.listen(process.env.PORT || 4242, () => 
+  console.log("🚀 Server running on http://localhost:4242")
+);
